bb16c4a4a3ed677637ae3f1327f186de1e39b47e
daveford <dford215@gmail.com>
2017-10-16 22:36:59 -0400

    set up structure for Blockchain service class

--- Services/Blockchain.cs
+++ Services/Blockchain.cs
@@ -1,9 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using CSharpBlockchain.Models;
 using CSharpBlockchain.Services.Interfaces;
 
 namespace CSharpBlockchain.Services
 {
     public class Blockchain : IBlockchain
     {
+        private List<Block> _Chain;
+        public int NewTransaction(string sender, string recipient, int amount)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public Block NewBlock(int proof, string prevHash)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Block LastBlock()
+        {
+            return _Chain[_Chain.Count - 2];
+        }
+
+        private byte[] GetHash(Block block)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Services/Interfaces/IBlockchain.cs
+++ Services/Interfaces/IBlockchain.cs
@@ -1,7 +1,11 @@
+using CSharpBlockchain.Models;
+
 namespace CSharpBlockchain.Services.Interfaces
 {
     public interface IBlockchain
     {
-        
+        int NewTransaction(string sender, string recipient, int amount);
+        Block NewBlock(int proof, string prevHash = "");
+        Block LastBlock();
     }
 }
